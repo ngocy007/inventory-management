0b14b7d836b4a63974347c1176b88bb1389cc197
WhatTheDuck <41006973+xuandat282@users.noreply.github.com>
2021-11-28 01:38:24 +0700

    all done

--- js/invoice.js
+++ js/invoice.js
@@ -1,3 +1,7 @@
+var overlay3 = document.getElementById("overlay3");
+var add_invoice = document.getElementById("add_invoice");
+var invoice_form = document.getElementById("invoice_form");
+var invoice_form_no = document.getElementById("invoice_form_no");
 var addsym = document.querySelector(".addsym");
 var warp_items = document.querySelector(".warp-items");
 
@@ -17,4 +21,20 @@ addsym.onclick = function () {
   </div>';
   addsym.before(items);
   warp_items.scrollTop = warp_items.scrollHeight;
+};
+
+
+add_invoice.onclick = function () {
+  console.log("sss");
+  overlay3.style.display = "block";
+  invoice_form.style.display = "block";
+};
+
+invoice_form_no.onclick = function () {
+  overlay3.style.display = "none";
+  invoice_form.style.display = "none";
+};
+overlay3.onclick = function () {
+  overlay3.style.display = "none";
+  invoice_form.style.display = "none";
 };
